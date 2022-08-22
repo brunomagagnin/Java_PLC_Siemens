fa709b5dfe24bfc5b0aa17a4afb78cbb03acd2ad
Bruno Magagnin Pereira <bruno_magagnin@yahoo.com.br>
2022-08-22 19:07:04 -0300

    Modificações: acrescentado try-catch aninhado para captura e exibir janela de erro.

--- src/com/clp/event/HoldStart.java
+++ src/com/clp/event/HoldStart.java
@@ -33,18 +33,16 @@ public class HoldStart implements ActionListener {
 	private DriverS7 driver;
 	private Timer timer;
 	private CycloStartState cycloStartState;
-	private JFrame jframe;
 
 	private boolean checkTrue = false;
 
 	public HoldStart(Button buttonStop, Button buttonTest, TextField fieldIP, CheckBox checkBoxHold,
-			CycloStartState cycloStartState, JFrame jframe) {
+			CycloStartState cycloStartState) {
 		this.buttonStop = buttonStop;
 		this.buttonTest = buttonTest;
 		this.fieldIP = fieldIP;
 		this.checkBoxHold = checkBoxHold;
 		this.cycloStartState = cycloStartState;
-		this.jframe = jframe;
 	}
 
 	@Override
@@ -52,50 +50,58 @@ public class HoldStart implements ActionListener {
 		// Bloco de monitoramento contínuo - Quando CheckBox "Monitoramento Contínuo"
 		// selecionado
 		try {
-			if (checkBoxHold.isSelected() && !cycloStartState.getState() && checkTrue) {
-				verifyCheck();
-				timer = new Timer();
-				driver = new DriverS7(fieldIP.getText());
-				checkBoxHold.setEnabled(false);
-				buttonStop.setEnabled(true);
-				cycloStartState.setState(true);
-				buttonTest.setEnabled(false);
+			try {
+				if (checkBoxHold.isSelected() && !cycloStartState.getState() && checkTrue) {
+					verifyCheck();
+					timer = new Timer();
+					driver = new DriverS7(fieldIP.getText());
+					checkBoxHold.setEnabled(false);
+					buttonStop.setEnabled(true);
+					cycloStartState.setState(true);
+					buttonTest.setEnabled(false);
 
-				timer.scheduleAtFixedRate(new TimerTask() {
-					@Override
-					public void run() {
-						driver.connect();
-						if (driver.isConnected()) {
-							for (int index = 0; index < listCheckBoolean.size(); index++) {
-								if (listCheckBoolean.get(index)) {
-									char i = FilterStringDB.getTypeMemory(listFieldDB.get(index).getText());
-									switch (i) {
-									case 'X':
-										driver.getBit(listFieldResult, listFieldDB, index);
-										break;
+					timer.scheduleAtFixedRate(new TimerTask() {
+						@Override
+						public void run() {
+							driver.connect();
+							if (driver.isConnected()) {
+								for (int index = 0; index < listCheckBoolean.size(); index++) {
+									if (listCheckBoolean.get(index)) {
+										char i = FilterStringDB.getTypeMemory(listFieldDB.get(index).getText());
+										switch (i) {
+										case 'X':
+											driver.getBit(listFieldResult, listFieldDB, index);
+											break;
 
-									case 'W':
-										driver.getWord(listFieldResult, listFieldDB, index);
-										break;
+										case 'W':
+											driver.getWord(listFieldResult, listFieldDB, index);
+											break;
 
-									case 'D':
-										driver.getDouble(listFieldResult, listFieldDB, index);
-										break;
+										case 'D':
+											driver.getDouble(listFieldResult, listFieldDB, index);
+											break;
+										}
 									}
 								}
+								driver.disconnect();
+							} else {
+								System.out.println("Sem conexão com CLP."); // Deve lançar uma exception.
 							}
-							driver.disconnect();
-						} else {
-							System.out.println("Sem conexão com CLP."); // Deve lançar uma exception.
 						}
-					}
-				}, 50, 500);
+					}, 50, 500);
+				}
+			} catch (VerifyException v) {
+
+			} finally {
+				JFrame frame = new JFrame();
+				JOptionPane.showMessageDialog(frame, "Verifique qual campo está preenchido errado.");
 			}
-		} catch (VerifyException | ArrayIndexOutOfBoundsException | NullPointerException v) {
-			
+
+		} catch (ArrayIndexOutOfBoundsException | NullPointerException exc) {
+			exc.printStackTrace();
 		} finally {
 			JFrame frame = new JFrame();
-			JOptionPane.showMessageDialog(frame, "Verifique qual campo está preenchido errado.");
+			JOptionPane.showMessageDialog(frame, "Ocorreu um erro inesperado, favor contatar o desenvolvedor.");
 		}
 	}
 
